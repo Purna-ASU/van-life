4b075c276d9a2e4e4f8466e45f1da544598eaeb8
Purna-ASU <ppeddire@asu.edu>
2023-09-07 17:29:07 -0700

    Removed the absolute paths

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -22,7 +22,7 @@ function Vans() {
 
     const vanElements = displayedCharacters.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            <Link to={`${van.id}`}>
             <img src={van.imageUrl} />
             <div className="van-info">
                 <h3>{van.name}</h3>
@@ -63,10 +63,12 @@ function Vans() {
                 <Link to={genNewSearchParamString("type", "rugged")}>Rugged</Link>
                 <Link to={genNewSearchParamString("type", null)}>Clear Filters</Link> */}
 
-                <button onClick={() => handleFilterChange("type", "simple")} className="van-type simple">Simple</button>
-                <button onClick={() => handleFilterChange("type", "luxury")} className="van-type luxury">Luxury</button>
-                <button onClick={() => handleFilterChange("type", "rugged")} className="van-type rugged">Rugged</button>
-                <button onClick={() => handleFilterChange("type", null)} className="van-type clear-filters">Clear Filters</button>
+                <button onClick={() => handleFilterChange("type", "simple")} className= {`van-type simple ${typeFilter === "simple" ? "selected" : ""}`} >Simple</button>
+                <button onClick={() => handleFilterChange("type", "luxury")} className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`} >Luxury</button>
+                <button onClick={() => handleFilterChange("type", "rugged")} className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`} >Rugged</button>
+                { typeFilter ?
+                    (<button onClick={() => handleFilterChange("type", null)} className="van-type clear-filters">Clear Filters</button>) :
+                    null }
             </div>
             <div className="van-list">
                 {vanElements}
