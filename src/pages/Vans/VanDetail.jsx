747af22c02ba6f0244cf748f0cbd17e7469612ae
Purna-ASU <ppeddire@asu.edu>
2023-09-08 19:16:28 -0700

    Added sad path to VanDetails

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -5,18 +5,34 @@ export default function VanDetail() {
     const params = useParams();
     const [van, setVan] = useState(null)
     const location = useLocation();
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        async function vanDetails() {
-            const res = await fetch(`/api/vans/${params.id}`)
-            const data = await res.json()
-            setVan(data.vans) 
+        async function loadVans() {
+            setLoading(true)
+            try{
+                const data = await res.json()
+                setVan(data) 
+            } catch(err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
         }
-        vanDetails()
+        loadVans()
     }, [params.id]) 
 
     const search = location.state?.search || ""
     const type = location.state?.type || "all"
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
 
     return (
         <div className="van-detail-container">
@@ -25,7 +41,7 @@ export default function VanDetail() {
                 relative="path"
                 className="back-button"
             >&larr; <span>{`Back to ${type} vans`}</span></Link>
-            {van ? (
+            {van && (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
                     <i className={`van-type ${van.type} selected`}>{van.type}</i>
@@ -33,8 +49,8 @@ export default function VanDetail() {
                     <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
                     <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
+                </div> 
+            )}
         </div>
     )
 }
