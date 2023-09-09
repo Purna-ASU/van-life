9389d97416f1b8450c977de437c61b0b9f10d0d8
Purna-ASU <ppeddire@asu.edu>
2023-09-09 14:38:57 -0700

    added the host dashboard

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -1,10 +1,11 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function AuthRequired() {
     const authenticated = true;
+    const location = useLocation();
 
     if(!authenticated) {
-        return <Navigate to="/login" state={{message: "You must log in first"}}/>
+        return <Navigate to="/login" state={{message: "You must log in first"}} replace/>
     }
 
     return <Outlet />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,13 +9,15 @@ export default function Login() {
     const location = useLocation();
     const navigate = useNavigate();
 
+    const from = location.state?.from || "/host"
+
     function handleSubmit(e) {
         e.preventDefault()
         setStatus("submitting")
         loginUser(loginFormData)
             .then(data => {
                 setError(null)
-                navigate("/host")
+                navigate(from, { replace: true})
             })
             .catch(err => {
                 setError(err)
